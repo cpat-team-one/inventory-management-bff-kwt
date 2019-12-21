a4bb3bb5145d488d23ef309e94b9f2e9e02c5cbb
KEATON TATOOLES <keaton.tatooles@ibm.com@KEATONs-MacBook-Pro.local>
2019-12-21 11:29:10 -0600

    Adds a mock service implementation

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,5 @@ export * from './project.api';
 export * from './project.service';
 export * from './task.api';
 export * from './task.service';
+export * from './stock-item.api';
+export * from './stock-items.mock.service';
